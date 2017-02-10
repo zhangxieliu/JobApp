acb92c12b106250993bea6e7cc40664314bcafad
zhangxieliu <Xie7925542908>
2017-02-10 10:10:59 +0800

    完成个人详情页的下拉图片扩大，actionbar的下拉渐进颜色渐变效果，增加一些小图标，链接职位详情页和页面跳转动画

--- app/src/main/java/com/fosu/jobapp/activity/JobDetailActivity.java
+++ app/src/main/java/com/fosu/jobapp/activity/JobDetailActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.fosu.jobapp.R;
+import com.zzhoujay.richtext.RichText;
+import com.zzhoujay.richtext.RichType;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +30,17 @@ public class JobDetailActivity extends Activity {
     ImageButton btnLove;
     @BindView(R.id.btn_collection)
     ImageButton btnCollection;
+    @BindView(R.id.tv_requirement)
+    TextView tvRequirement;
+    private String text = "<h1>测试标题</h1>\n" +
+            "<h4>SwipeRefreshLayout里面需要注意的Api：</h4>\n" +
+            "<p>1.RefreshListener(OnRefreshListener listener)  设置下拉监听，当用户下拉的时候会去执行回调<br />\n" +
+            "2.setColorSchemeColors(int... colors) 设置 进度条的颜色变化，最多可以设置4种颜色<br />\n" +
+            "3.setProgressViewOffset(boolean scale, int start, int end) 调整进度条距离屏幕顶部的距离<br />\n" +
+            "4.setRefreshing(boolean refreshing) 设置SwipeRefreshLayout当前是否处于刷新状态，一般是在请求数据的时候设置为true，在数据被加载到View中后，设置为false<br />\n" +
+            "SwipeRefreshLayout 是谷歌公司推出的用于下拉刷新的控件，SwipeRefreshLayout已经被放到了sdk中，在Version 19.1之后SwipeRefreshLayout 被放到support v4中。\n" +
+            "源码在SDK\\sdk\\extras\\android\\support\\v4\\src\\java\\android\\support\\v4\\widget\\SwipeRefreshLayout.java</p>\n" +
+            "<p>谷歌公司只提供了下拉刷新的功能，RecyclerView的出现基本就是为了替代ListView，GridView的。</p>";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +49,11 @@ public class JobDetailActivity extends Activity {
         getActionBar().hide();
         ButterKnife.bind(this);
         tagGroup.setTags(new String[]{"Android", "Java", "SQLite", "Html"});
+        RichText.from(text)
+                .type(RichType.HTML)
+                .autoFix(true)  //是否自动修复，默认为true
+                .bind(this) // 绑定richText对象到某个object上，方便后面的清理
+                .into(tvRequirement);
     }
 
     @OnClick({R.id.btn_back, R.id.btn_love, R.id.btn_collection})
@@ -49,5 +68,17 @@ public class JobDetailActivity extends Activity {
             case R.id.btn_collection:
                 break;
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.in_from_left, R.anim.out_to_right);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        RichText.clear(this);
     }
 }
